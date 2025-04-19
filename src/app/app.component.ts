a48b043997d592c9f8f298b0713a66648b19d208
david esteve <david.esteve66@gmail.com>
2025-04-19 11:00:41 +0200

    implementation du service movie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from './shared/nav-bar/nav-bar.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, UpperCasePipe, LowerCasePipe, TitleCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cinephoriaWeb';
+  title = 'cinephoria';
+  subTittle = ' un cinema qui ne fait pas son cinema !!! '
 }
