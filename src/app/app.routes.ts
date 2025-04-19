3cccf61c42c6701e53af133731ca0d503e346bf8
david esteve <david.esteve66@gmail.com>
2025-04-19 11:54:43 +0200

    ajout de la sou page statistique et des graphique

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './view/home/home.component';
+import {StatisticComponent} from './shared/dashboard/statistic/statistic.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path:'home', component: HomeComponent }
+  { path:'home', component: HomeComponent },
+  { path:'dashboard', component: StatisticComponent ,
+    children : [
+      { path: '', redirectTo: '/dashboard/statistic', pathMatch: 'full' },
+      { path:'statistic', component: StatisticComponent }
+
+    ]
+  }
 ];
